7d3a93f5f21c9ab10778a0014073d940af6bafd5
jafet <jafetpintle@gmail.com>
2022-09-28 23:00:32 -0500

    Move just a type of file

--- FManager.py
+++ FManager.py
@@ -1,9 +1,11 @@
+from re import S
 import shutil
 import os
 import ctypes
 
 path_src = r'Files\Documentos'
 path_dst = r'Files\Destino'
+file_extension = '.jpg'
 
 def move_files(files_names):
     '''
@@ -13,8 +15,7 @@ def move_files(files_names):
     for file in files_names:
         shutil.move(path_src+'\\'+file, path_dst)
     
-
-def get_files(dir_path: str):
+def get_files(dir_path: str, ext: str):
     '''
     Funtion to walk across a directory an return
     all the files inside
@@ -26,15 +27,30 @@ def get_files(dir_path: str):
     for path in os.listdir(dir_path):
         # check if current path is a file
         if os.path.isfile(os.path.join(dir_path, path)):
-            files.append(path)
+            if(has_extension(ext, path)):
+                files.append(path)
     return files
     
+def has_extension(ext: str, file: str)->bool:
+    '''
+    Check if my file is of a specific extension
+    '''
+    # this will return a tuple of root and extension
+    split_file = os.path.splitext(file)
+    #Return true if is the same extension
+    return ext == split_file[1] 
 
+def print_files(files):
+    for file in files:
+        print(f'File: {file}')
 
 def run():
-    files = get_files(path_src)
+    files = get_files(path_src, file_extension)
+    print('You will move the following files: ')
+    print_files(files)
+    print('-----')
     move_files(files)
-    print('Files has been moved!')
+    print(f'Files of type {file_extension} has been moved!')
 
 if __name__ == "__main__":
     run()
