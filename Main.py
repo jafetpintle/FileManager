6f9af239f785c1d19e0ec24891f3a3e262a2245f
jafet <jafetpintle@gmail.com>
2022-10-01 01:23:31 -0500

    Gui style implementation

--- Main.py
+++ Main.py
@@ -1,62 +1,45 @@
 import tkinter as tk
-from tkinter import ttk
-
-
-def create_buttons_frame(container):
-    frame = ttk.Frame(container)
-
-    
-    #Buttons setup
-    ttk.Button(frame, text='Select folder').grid(column=1,row=0 , padx=10, pady=5)
-    ttk.Button(frame, text='Select folder').grid(column=1,row=1, padx=10, pady=5)
-
-    return frame
-
-def create_input_frame(container):
-    frame = ttk.Frame(container)
-    #grid layout for the input frame
-    frame.columnconfigure(0, weight=1)
-    frame.columnconfigure(0, weight=3)
-
-    #Source folder
-    
-
-    #Destination folder
 
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        colors = {'primary':'#2b2d42', 'secundary':'#d90429', 'text1':'#edf2f4'}
 
-
+        self['background'] = colors['primary']
+        #Def button font
+        font_button =  ('Trebuchet MS', 15)
+        font_label = ('Trebuchet MS', 20)
+        font_entry = ('Trebuchet MS', 20)
+        font_text = ('Trebuchet MS', 15)
         
 
         #Source folder
-        ttk.Label(self, text='Source: ').grid(column=0, row=0, padx=5, pady=5, sticky=tk.W)
-        entry_source = ttk.Entry(self, width=35)
+        tk.Label(self, text='Move from: ', font=font_label, fg=colors['text1'], bg=colors['primary']).grid(column=0, row=0, padx=5, pady=5, sticky=tk.W)
+        entry_source = tk.Entry(self, width=35, font=font_entry)
         entry_source.grid(column=1,row=0,sticky=tk.W)
-        ttk.Button(self, text='Select folder').grid(column=2,row=1, padx=10, pady=5, sticky=tk.W)
+        tk.Button(self, text='Select folder', bg=colors['secundary'], fg=colors['text1'], font=font_button).grid(column=2,row=1, padx=10, pady=5, sticky=tk.W)
 
         #Destination folder
-        ttk.Label(self, text='Destination: ').grid(column=0, row=1, padx=5, pady=5, sticky=tk.W)
-        destinarion_source = ttk.Entry(self, width=35)
+        tk.Label(self, text='To: ', font=font_label, fg=colors['text1'], bg=colors['primary']).grid(column=0, row=1, padx=5, pady=5, sticky=tk.W)
+        destinarion_source = tk.Entry(self, width=35, font=font_entry)
         destinarion_source.grid(column=1, row=1, sticky=tk.W)
-        ttk.Button(self, text='Select folder').grid(column=2,row=0 , padx=10, pady=5, sticky=tk.W)
+        tk.Button(self, text='Select folder', bg=colors['secundary'], fg=colors['text1'], font=font_button).grid(column=2,row=0 , padx=10, pady=5, sticky=tk.W)
 
         #Extension and move files
-        ttk.Label(self, text='Extension: ').grid(column=0,row=2, padx=5, pady=5,sticky=tk.W)
-        extension = ttk.Entry(self, width=5)
+        tk.Label(self, text='Type file: ', font=font_label, fg=colors['text1'], bg=colors['primary']).grid(column=0,row=2, padx=5, pady=5,sticky=tk.W)
+        extension = tk.Entry(self, width=5, font=font_entry)
         extension.grid(column=1,row=2, sticky=tk.W)
 
         #Find and move buttons
-        ttk.Button(self, text='Move files').grid(column=2, row=4, padx=10,pady=40, sticky=tk.NE)
-        ttk.Button(self, text='Find files').grid(column=2,row=4, padx=10, sticky=tk.NE)
+        tk.Button(self, text='Move files', bg=colors['secundary'], fg=colors['text1'], font=font_button, width=11).grid(column=2, row=4, padx=10,pady=70, sticky=tk.NE)
+        tk.Button(self, text='Find files', bg=colors['secundary'], fg=colors['text1'], font=font_button,width=11).grid(column=2,row=4, padx=10, sticky=tk.NE)
 
         #Files
-        ttk.Label(self,text='Files: ').grid(column=0, row=4, padx=5,pady=5, sticky=tk.NW)
-        files = tk.Text(self, height=15, width=30)
-        scroll = ttk.Scrollbar(self, orient='vertical', command=files.yview)
+        tk.Label(self,text='Files: ', font=font_label, fg=colors['text1'], bg=colors['primary']).grid(column=0, row=4, padx=5,pady=5, sticky=tk.NW)
+        files = tk.Text(self, height=15, width=50,  font=font_text)
+        scroll = tk.Scrollbar(self, orient='vertical', command=files.yview)
         files['yscrollcommand'] = scroll.set
         files.grid(column=1, row=4, pady=10, sticky=tk.W)
         scroll.grid(column=1, row=4, pady=10, sticky="nse")
